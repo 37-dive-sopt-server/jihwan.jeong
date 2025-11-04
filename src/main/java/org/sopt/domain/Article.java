c0fb1a4eb6dd6618736fdeba4604ba231b0f9902
JJhwan7099 <ghks7099@naver.com>
2025-11-05 02:03:18 +0900

    feat(Article): 아티클 엔티티 구현

--- src/main/java/org/sopt/domain/Article.java
+++ src/main/java/org/sopt/domain/Article.java
@@ -2,12 +2,20 @@ package org.sopt.domain;
 
 import jakarta.persistence.*;
 
+import java.time.LocalDate;
+
 @Entity
 public class Article {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    private Tag tag;
+    private LocalDate createdAt;
+
+    @Column(unique = true)
+    private String title;
+    private String content;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "memberId")
