4dc54df92d91081d13b25c0f182aca59e3b32617
JJhwan7099 <ghks7099@naver.com>
2025-11-05 02:10:56 +0900

    feat(ArticleRepository): Article 레포지토리 생성

--- src/main/java/org/sopt/repository/MemberRepository.java
+++ src/main/java/org/sopt/repository/MemberRepository.java
@@ -6,8 +6,6 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
-//    boolean isExistEmail(String email);
-//    boolean isExistId(Long id);
     boolean existsByEmail(String email);
     boolean existsById(Long id);
 }
