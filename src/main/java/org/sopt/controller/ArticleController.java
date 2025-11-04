5813fe59ce1d45560e1378cbd1fc61edd9934157
JJhwan7099 <ghks7099@naver.com>
2025-11-05 02:50:07 +0900

    feat(ArticleController): Article 컨트롤러 구현

--- src/main/java/org/sopt/controller/ArticleController.java
+++ src/main/java/org/sopt/controller/ArticleController.java
@@ -1,9 +1,47 @@
 package org.sopt.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.AllArgsConstructor;
+import org.sopt.dto.article.ArticleRequestDto;
+import org.sopt.dto.article.ArticleResponseDto;
+import org.sopt.global.ApiResponse;
+import org.sopt.service.ArticleService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/article")
+@AllArgsConstructor
 public class ArticleController {
+    private final ArticleService articleService;
+
+    @PostMapping
+    public ResponseEntity<?> createArticle(
+            @RequestBody ArticleRequestDto.create requestDto
+    ) {
+        ArticleResponseDto.ArticleInfo body = articleService.createArticle(requestDto);
+
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(new ApiResponse<>(201, "아티클 생성 성공", body));
+    }
+
+    @GetMapping
+    public ResponseEntity<?> findAllArticles() {
+        ArticleResponseDto.ArticleInfos body = articleService.findAllArticles();
+
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(new ApiResponse<>(200, "전제 아티클 조회 성공", body));
+    }
+
+    @GetMapping("/{articleId}")
+    public ResponseEntity<?> findArticleById(
+            @PathVariable Long articleId
+    ) {
+        ArticleRequestDto.findArticleByArticleId requestDto = new ArticleRequestDto.findArticleByArticleId(articleId);
+        ArticleResponseDto.ArticleInfo body = articleService.findArticleByArticleId(requestDto);
+
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(new ApiResponse<>(200,"아티클 조회 성공",body));
+    }
+
 }
